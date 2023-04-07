0254ac7c59624e8fd947fb532e0d38915412192f
Mahmoud <barghoutmahmoud2@gmail.com>
2023-04-08 01:18:57 +0200

    created the initial landing page and added some styling using tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <h1>welcome to tome heap</h1>
+    <div className="text-center bg-[url(./image_resources/booksBackground.jpg)] bg-cover h-screen">
+      <h1 className="mt-auto text-4xl text-white">welcome to tome heap</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
