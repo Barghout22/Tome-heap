27d573d65041568da2e51114873b70e88bb36d2a
Mahmoud <barghoutmahmoud2@gmail.com>
2023-04-07 16:59:47 +0200

    created basic file layout with vite

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>welcome to tome heap</h1>
+    </div>
+  )
+}
+
+export default App
