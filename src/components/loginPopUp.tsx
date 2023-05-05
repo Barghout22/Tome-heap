c737940bb4678c2697600b3b8eb2ad94569177be
Mahmoud <barghoutmahmoud2@gmail.com>
2023-05-05 15:55:25 +0300

    added error display on login\sign up. extracted error handling into the async sign up and login functions in the login pop up, and created an error status prop and error message prop that get changed and displayed on the pop up in case of an error. Also added a message to display in case of successful sign up.

--- src/components/loginPopUp.tsx
+++ src/components/loginPopUp.tsx
@@ -24,6 +24,8 @@ const LogInPopUp = ({
   const [userName, setUserName] = useState(" ");
   const [email, setEmail] = useState(" ");
   const [password, setPassword] = useState(" ");
+  const [errorStatus, setErrorStatus] = useState(" ");
+  const [errorMessage, setErrorMessage] = useState(" ");
   const auth = getAuth();
   async function signInWithGoogle() {
     const provider = new GoogleAuthProvider();
@@ -40,40 +42,51 @@ const LogInPopUp = ({
     try {
       await createUserWithEmailAndPassword(auth, email, pwd).then(() => {
         updateProfile(auth.currentUser!, { displayName: userName });
-        // console.log(auth.currentUser?.displayName);
         signOut(auth);
+        setErrorStatus("no error");
+        setLogInStatus("log in");
+        setTimeout(() => {
+          setErrorStatus(" ");
+          setErrorMessage(" ");
+        }, 2000);
       });
-    } catch (e) {
-      console.log(e);
+    } catch (e: any) {
+      // console.log(e.message);
+      setErrorStatus("error");
+      setErrorMessage(e.message);
+      setTimeout(() => {
+        setErrorStatus(" ");
+        setErrorMessage(" ");
+      }, 2000);
     }
   }
   async function signInWithEmail(email: string, pwd: string) {
-    await signInWithEmailAndPassword(auth, email, pwd).then(() => {
-      // console.log(auth.currentUser?.displayName);
-      setUserSignInStatus(true);
-    });
+    try {
+      await signInWithEmailAndPassword(auth, email, pwd).then(() => {
+        // console.log(auth.currentUser?.displayName);
+        setLogInStatus("none");
+        setUserSignInStatus(true);
+      });
+    } catch (e: any) {
+      // console.log(e.message);
+      setErrorStatus("error");
+      setErrorMessage(e.message);
+      setTimeout(() => {
+        setErrorStatus(" ");
+        setErrorMessage(" ");
+      }, 2000);
+    }
   }
 
   function handleSignInLogIn() {
+    let errorState = false;
     if (email !== " " && password !== " ") {
       if (signUp) {
         if (userName !== " ") {
-          try {
-            createUserWithEmail(email, password, userName);
-          } catch (e) {
-            console.log(e);
-            return;
-          }
-          setLogInStatus("none");
+          createUserWithEmail(email, password, userName);
         }
       } else {
-        try {
-          signInWithEmail(email, password);
-        } catch (e) {
-          console.log(e);
-          return;
-        }
-        setLogInStatus("none");
+        signInWithEmail(email, password);
       }
     }
   }
@@ -96,6 +109,16 @@ const LogInPopUp = ({
           <h2 className="my-3 text-3xl font-Pacifico ">tH</h2>
         </div>
         <p>*all fields are required to proceed</p>
+        {errorStatus === "error" && (
+          <p className="text-red-900 bg-red-400 bg-opacity-25">
+            {errorMessage}
+          </p>
+        )}
+        {errorStatus === "no error" && (
+          <p className="text-green-900 bg-green-400 bg-opacity-25">
+            "sign up successful. Please sign in to contiune"
+          </p>
+        )}
         {signUp && (
           <input
             required
@@ -126,6 +149,19 @@ const LogInPopUp = ({
             setPassword(e.target.value);
           }}
         />
+
+        <button
+          className="bg-black text-white w-10/12 h-9 hover:bg-gray-800 "
+          onClick={handleSignInLogIn}
+        >
+          {status}
+        </button>
+        <button
+          className="bg-amber-500 text-white w-10/12 h-9 hover:bg-amber-400 "
+          onClick={signInWithGoogle}
+        >
+          {status} with google
+        </button>
         {signUp && (
           <p>
             Already have an account?
@@ -148,19 +184,6 @@ const LogInPopUp = ({
             </span>
           </p>
         )}
-
-        <button
-          className="bg-black text-white w-10/12 h-9 hover:bg-gray-800 "
-          onClick={handleSignInLogIn}
-        >
-          {status}
-        </button>
-        <button
-          className="bg-amber-500 text-white w-10/12 h-9 hover:bg-amber-400 "
-          onClick={signInWithGoogle}
-        >
-          {status} with google
-        </button>
       </div>
     </div>
   );
