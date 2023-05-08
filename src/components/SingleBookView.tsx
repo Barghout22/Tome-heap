71dfb45ce557a5969b9c9f9df98c3084e4934cec
Mahmoud <barghoutmahmoud2@gmail.com>
2023-05-08 03:11:02 +0300

    added some review functionality. in the single book view component, added some functionality to the inputs for reviews to save the reviews under the book and under the users id. on mount a check is performed to see if the book has any user reviews on it and retrieve them to be displayed.

--- src/components/SingleBookView.tsx
+++ src/components/SingleBookView.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { BookInfo } from "./BookListView";
+import profileImagePlaceHolder from "../image_resources/userDefaultImage.png";
 import {
   getFirestore,
   collection,
   query,
   where,
   getDocs,
+  setDoc,
+  doc,
+  serverTimestamp,
 } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-
 import AddBookToUserListBtn from "./addBookToUsrListBtn";
 
 const checkForBook = async (id: string) => {
@@ -23,6 +26,60 @@ const checkForBook = async (id: string) => {
   querySnapshot.docs.length > 0 ? (returnVal = true) : (returnVal = false);
   return returnVal;
 };
+const checkForReviews = async (
+  bookId: string,
+  setPreviousBookReviews: Function,
+  setUserHasRviewd: Function
+) => {
+  const BookReviewCluster = `book-${bookId}-reviews`;
+  const q = query(collection(getFirestore(), BookReviewCluster));
+  let reviewListPLaceHolder: any[] = [];
+  await getDocs(q).then((results) => {
+    const resultHolder = results.docs;
+    resultHolder.forEach((review) => {
+      let infoHolderObj = {
+        userId: review.data().userId,
+        bookId: review.data().bookId,
+        rating: review.data().rating,
+        review: review.data().review,
+        timeStamp: review.data().timeStamp,
+      };
+      getAuth().currentUser?.uid === review.data().userId
+        ? setUserHasRviewd(true)
+        : null;
+
+      console.log(getFirestore());
+      reviewListPLaceHolder.push(infoHolderObj);
+    });
+    console.log(reviewListPLaceHolder);
+    setPreviousBookReviews(reviewListPLaceHolder);
+    return reviewListPLaceHolder;
+  });
+};
+
+const addReview = async (
+  userId: string,
+  rating: number,
+  bookId: string,
+  review?: string
+) => {
+  const reviewDoc = {
+    userId: userId,
+    bookId: bookId,
+    rating: rating,
+    review: review,
+    timestamp: serverTimestamp(),
+  };
+  await setDoc(
+    doc(getFirestore(), `book-${bookId}-reviews`, `user-${userId}-review`),
+    reviewDoc
+  );
+  await setDoc(
+    doc(getFirestore(), `user-${userId}-reviews`, `book-${bookId}-review`),
+    reviewDoc
+  );
+};
+
 const SingleBookView = ({
   bookData,
   userSignInStatus,
@@ -33,18 +90,32 @@ const SingleBookView = ({
   setLogInStatus: Function;
 }) => {
   const [bookPresentInUserList, setBookPresentInUserList] = useState(false);
+  const [averageRating, setAverageRating] = useState(0);
   const [bookStarRating, setBookStarRating] = useState(0);
   const [bookReview, setBookReview] = useState(" ");
+  const [userHasRviewd, setUserHasRviewd] = useState(false);
+  const [previousBookReviews, setPreviousBookReviews] = useState([
+    {
+      userName: " ",
+      profilePicSource: " ",
+      userId: " ",
+      timeStamp: " ",
+      rating: 0,
+      review: " ",
+    },
+  ]);
   useEffect(() => {
     if (userSignInStatus) {
       checkForBook(bookData.id).then((value) => {
         setBookPresentInUserList(value);
       });
     }
+    checkForReviews(bookData.id, setPreviousBookReviews, setUserHasRviewd);
   }, []);
   const handleRatingInput = (e: any) => {
     if (userSignInStatus) {
       console.log(e.target.value);
+      setBookStarRating(e.target.value);
     } else {
       setLogInStatus("sign up");
     }
@@ -52,7 +123,12 @@ const SingleBookView = ({
   const handleReviewInput = (e: any) => {
     e.preventDefault();
     if (userSignInStatus) {
-      console.log(e.target.value);
+      addReview(
+        getAuth().currentUser!.uid,
+        bookStarRating,
+        bookData.id,
+        bookReview !== " " ? bookReview : undefined
+      );
     } else {
       setLogInStatus("sign up");
     }
@@ -66,6 +142,13 @@ const SingleBookView = ({
           <h2 className="text-2xl">{bookData.author}</h2>
           <h3 className="text-2xl">{bookData.pageNo} pages</h3>
           <p className="text-xl">{bookData.description}</p>
+          <p>
+            current average rating:{" "}
+            {averageRating > 0
+              ? `${averageRating} stars`
+              : "no ratings on this book"}
+            stars
+          </p>
           <AddBookToUserListBtn
             userSignInStatus={userSignInStatus}
             setLogInStatus={setLogInStatus}
@@ -75,70 +158,72 @@ const SingleBookView = ({
           />
         </div>
       </div>
-      <p className="text-2xl font-semibold ml-4 mt-5">
-        rate this book from 1 to 5 stars
-      </p>
-      <span className="flex w-1/2 justify-around">
-        <label>
-          <input
-            type="radio"
-            name="book-rating"
-            value="1"
-            onClick={handleRatingInput}
-          />
-          1 star
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="book-rating"
-            value="2"
-            onClick={handleRatingInput}
-          />
-          2 stars
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="book-rating"
-            value="3"
-            onClick={handleRatingInput}
-          />
-          3 stars
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="book-rating"
-            value="4"
-            onClick={handleRatingInput}
-          />
-          4 stars
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="book-rating"
-            value="5"
-            onClick={handleRatingInput}
-          />
-          5 stars
-        </label>
-      </span>
 
       {userSignInStatus && (
         <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log("review is being processed");
-          }}
+          onSubmit={handleReviewInput}
           className="flex flex-col w-2/3 mt-12"
         >
+          <p className="text-2xl font-semibold ml-4 mt-5">
+            rate this book from 1 to 5 stars
+          </p>
+          <span className="flex w-1/2 justify-around">
+            <label>
+              <input
+                type="radio"
+                name="book-rating"
+                value="1"
+                required
+                onClick={handleRatingInput}
+              />
+              1 star
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="book-rating"
+                value="2"
+                onClick={handleRatingInput}
+              />
+              2 stars
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="book-rating"
+                value="3"
+                onClick={handleRatingInput}
+              />
+              3 stars
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="book-rating"
+                value="4"
+                onClick={handleRatingInput}
+              />
+              4 stars
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="book-rating"
+                value="5"
+                onClick={handleRatingInput}
+              />
+              5 stars
+            </label>
+          </span>
+
           <textarea
             cols={30}
             rows={10}
             className="border-2 border-white bg-gray-800 text-white font-Lobster text-2xl p-4"
             placeholder="write a review about this book"
+            onChange={(e) => {
+              setBookReview(e.target.value);
+            }}
           ></textarea>
           <button
             type="submit"
