9a150f764690515da22b19ff79368cc73beed94d
Mahmoud <barghoutmahmoud2@gmail.com>
2023-11-03 01:16:55 +0200

    adjusted how friend request and friends are saved. now User data such as user name and profile picture for both users is fetched and included in the friend request objects and the friend objects.

--- src/components/userPageInterface.tsx
+++ src/components/userPageInterface.tsx
@@ -3,17 +3,26 @@ import { useNavigate } from "react-router-dom";
 import { getAuth } from "firebase/auth";
 import {
   getFirestore,
-  collection,
-  addDoc,
   setDoc,
   getDoc,
-  query,
-  where,
-  getDocs,
   deleteDoc,
   doc,
 } from "firebase/firestore";
-import { async } from "@firebase/util";
+import { userDefaultImage } from "../RouteSwitch";
+
+const getUsers = async (currentUserId: string, viewedUserId: string) => {
+  try {
+    const currentUser = await getDoc(
+      doc(getFirestore(), "usersData", `user-${currentUserId}`)
+    );
+    const viewedUser = await getDoc(
+      doc(getFirestore(), "usersData", `user-${viewedUserId}`)
+    );
+    return [currentUser.data(), viewedUser.data()];
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const retrieveFriendStatus = async (
   currentUserId: string,
@@ -60,8 +69,34 @@ const UserpageInterface = ({
   const navigate = useNavigate();
   const [friendStatus, setFriendStatus] = useState<boolean | undefined>();
   const [friendReqStatus, setFriendReqStatus] = useState<string | undefined>();
+  const [currentUser, setCurrentUser] = useState({
+    username: " ",
+    photoUrl: " ",
+    viewedUserId: currentUserId,
+  });
+  const [viewedUser, setViewedUser] = useState({
+    username: " ",
+    photoUrl: " ",
+    userId: viewedUserId,
+  });
 
   useEffect(() => {
+    getUsers(currentUserId!, viewedUserId).then((userData) => {
+      setCurrentUser({
+        username: userData![0]?.username,
+        photoUrl: userData![0]?.profilePicture
+          ? userData![0].profilePicture
+          : userDefaultImage,
+        viewedUserId: currentUserId,
+      });
+      setViewedUser({
+        username: userData![1]?.username,
+        photoUrl: userData![1]?.profilePicture
+          ? userData![1].profilePicture
+          : userDefaultImage,
+        userId: viewedUserId,
+      });
+    });
     retrieveFriendStatus(currentUserId!, viewedUserId).then((friendStatus) => {
       setFriendStatus(friendStatus);
       if (!friendStatus) {
@@ -84,25 +119,25 @@ const UserpageInterface = ({
     navigate("/messages");
   };
   const removeFriend = async () => {
-       try {
-         await deleteDoc(
-           doc(
-             getFirestore(),
-             `user-${currentUserId}-friends`,
-             `user-${viewedUserId}`
-           )
-         );
-         await deleteDoc(
-           doc(
-             getFirestore(),
-             `user-${viewedUserId}-friends`,
-             `user-${currentUserId}`
-           )
-         );
-         setFriendStatus(undefined);
-       } catch (e) {
-         console.error(e);
-       }
+    try {
+      await deleteDoc(
+        doc(
+          getFirestore(),
+          `user-${currentUserId}-friends`,
+          `user-${viewedUserId}`
+        )
+      );
+      await deleteDoc(
+        doc(
+          getFirestore(),
+          `user-${viewedUserId}-friends`,
+          `user-${currentUserId}`
+        )
+      );
+      setFriendStatus(undefined);
+    } catch (e) {
+      console.error(e);
+    }
   };
   const addFriend = async () => {
     try {
@@ -112,7 +147,12 @@ const UserpageInterface = ({
           `user-${currentUserId}-friendReqs`,
           `user-${viewedUserId}`
         ),
-        { status: "sent", viewed: true }
+        {
+          username: viewedUser.username,
+          profilePicture: viewedUser.photoUrl,
+          status: "sent",
+          viewed: true,
+        }
       );
       await setDoc(
         doc(
@@ -120,7 +160,12 @@ const UserpageInterface = ({
           `user-${viewedUserId}-friendReqs`,
           `user-${currentUserId}`
         ),
-        { status: "received", viewed: false }
+        {
+          username: currentUser.username,
+          profilePicture: currentUser.photoUrl,
+          status: "received",
+          viewed: false,
+        }
       );
 
       setFriendReqStatus("sent");
@@ -136,7 +181,11 @@ const UserpageInterface = ({
           `user-${currentUserId}-friends`,
           `user-${viewedUserId}`
         ),
-        { timeStamp: new Date().toLocaleString() }
+        {
+          username: viewedUser.username,
+          profilePicture: viewedUser.photoUrl,
+          timeStamp: new Date().toLocaleString(),
+        }
       );
       await setDoc(
         doc(
@@ -144,7 +193,11 @@ const UserpageInterface = ({
           `user-${viewedUserId}-friends`,
           `user-${currentUserId}`
         ),
-        { timeStamp: new Date().toLocaleString() }
+        {
+          username: currentUser.username,
+          profilePicture: currentUser.photoUrl,
+          timeStamp: new Date().toLocaleString(),
+        }
       );
       setFriendStatus(true);
       cancelRequest();
@@ -175,73 +228,74 @@ const UserpageInterface = ({
   };
 
   return (
-     getAuth().currentUser&&
-    <>
-      {friendStatus && (
-        <div className="bg-gray-800 flex sm:flex-row justify-start items-center mt-9 flex-col">
-          <p
-            className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
-            onClick={viewBooks}
-          >
-            {username.split(" ")[0]}'s books
-          </p>
-          <p
-            className="text-black rounded-full  mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
-            onClick={viewReviews}
-          >
-            {username.split(" ")[0]}'s book reviews
-          </p>
-          <p
-            className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
-            onClick={sendMessage}
-          >
-            message {username.split(" ")[0]}
-          </p>
-          <p
-            className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
-            onClick={removeFriend}
-          >
-            remove friend
-          </p>
-        </div>
-      )}
-      {!friendStatus && friendReqStatus === "received" && (
-        <div className="bg-gray-800 flex sm:flex-row justify-start items-center mt-9 flex-col">
-          <p
-            className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
-            onClick={acceptRequest}
-          >
-            accept friend request
-          </p>
-          <p
-            className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
-            onClick={cancelRequest}
-          >
-            reject friend request
-          </p>
-        </div>
-      )}
-      {!friendStatus && friendReqStatus === "sent" && (
-        <div className="bg-gray-800 flex sm:flex-row justify-center items-center mt-9 flex-col">
-          <p
-            className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
-            onClick={cancelRequest}
-          >
-            cancel friend request
-          </p>
-        </div>
-      )}
-      {!friendStatus && !friendReqStatus && (
-        <div className="bg-gray-800 flex sm:flex-row justify-center items-center mt-9 flex-col">
-          <p
-            className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
-            onClick={addFriend}
-          >
-            add friend
-          </p>
-        </div>
-      )}
-    </>
+    getAuth().currentUser && (
+      <>
+        {friendStatus && (
+          <div className="bg-gray-800 flex sm:flex-row justify-start items-center mt-9 flex-col">
+            <p
+              className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
+              onClick={viewBooks}
+            >
+              {username.split(" ")[0]}'s books
+            </p>
+            <p
+              className="text-black rounded-full  mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
+              onClick={viewReviews}
+            >
+              {username.split(" ")[0]}'s book reviews
+            </p>
+            <p
+              className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
+              onClick={sendMessage}
+            >
+              message {username.split(" ")[0]}
+            </p>
+            <p
+              className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
+              onClick={removeFriend}
+            >
+              remove friend
+            </p>
+          </div>
+        )}
+        {!friendStatus && friendReqStatus === "received" && (
+          <div className="bg-gray-800 flex sm:flex-row justify-start items-center mt-9 flex-col">
+            <p
+              className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
+              onClick={acceptRequest}
+            >
+              accept friend request
+            </p>
+            <p
+              className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
+              onClick={cancelRequest}
+            >
+              reject friend request
+            </p>
+          </div>
+        )}
+        {!friendStatus && friendReqStatus === "sent" && (
+          <div className="bg-gray-800 flex sm:flex-row justify-center items-center mt-9 flex-col">
+            <p
+              className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
+              onClick={cancelRequest}
+            >
+              cancel friend request
+            </p>
+          </div>
+        )}
+        {!friendStatus && !friendReqStatus && (
+          <div className="bg-gray-800 flex sm:flex-row justify-center items-center mt-9 flex-col">
+            <p
+              className="text-black rounded-full mt-4 text-3xl font-semibold w-52 mb-8 mx-2 text-center bg-white transition-all hover:text-white hover:bg-black cursor-pointer "
+              onClick={addFriend}
+            >
+              add friend
+            </p>
+          </div>
+        )}
+      </>
+    )
   );
 };
 export default UserpageInterface;
